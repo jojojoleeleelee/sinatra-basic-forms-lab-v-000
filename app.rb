530dddd248d7edefdf71de4be8779d658eb0e550
Joanne Lee <vernapii@gmail.com>
2018-05-02 06:23:06 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ class App < Sinatra::Base
   end
 
   post '/display_puppy' do
-    params[:name],params[:breed],params[:age]
+    Puppy.new(params[:name], params[:breed], params[:age])
     erb :display_puppy
   end
 end
